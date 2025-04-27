2b0181bd7ce0852a8593212622bfdc2b90708bd6
Brunofell <brunofelimarti@gmail.com>
2025-04-27 00:56:10 -0300

    first commit

---
+++ src/test/java/com/example/FURIA/FuriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FURIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
