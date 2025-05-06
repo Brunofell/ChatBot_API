3c8456de56a1c283269099024a57498395fc7254
Brunofell <brunofelimarti@gmail.com>
2025-05-06 20:33:08 -0300

    cors

--- src/main/java/com/example/CHATBOT/AssistantController.java
+++ src/main/java/com/example/CHATBOT/AssistantController.java
@@ -21,7 +21,7 @@ public class AssistantController {
     @GetMapping("/music")
     public Flux<String> musicChatConfig(@RequestParam(value = "message", defaultValue = "De saudações ao usuário e pergunte o que ele deseja.") String message) {
 
-        String systemInstruction = "Você é um membro da equipe da plataforma ChatBot, uma plataforma que fala sobre o mundo da música. Responda de forma empolgante e descontraída como se você fosse um guitarrista muito louco!";
+        String systemInstruction = "Você é um membro da equipe da plataforma ChatBot, uma plataforma que fala sobre o mundo da música. Responda de forma descontraída mas informativa também";
         String systemRestriction = "Só responda coisas baseadas em música. Se a pergunta recebida tiver a ver com outro assunto que não seja música, explique que você não pode responder de forma bem-humorada.";
 
         String finalPrompt = """
